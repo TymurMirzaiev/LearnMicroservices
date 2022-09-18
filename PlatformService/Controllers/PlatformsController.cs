84a25d1a346f9c968fe226e2677aa801c640394a
Tymur_Mirzaiev <tymur.mirzaiev@wolterskluwer.com>
2022-09-18 19:25:27 +0300

    add a few endpoints to manipulate platform entities

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+    private readonly IPlatformRepo _repository;
+    private readonly IMapper _mapper;
+
+    public PlatformsController(IPlatformRepo repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetAllPlatforms()
+    {
+        Console.Write("--> Getting Platforms...");
+
+        var platforms = _repository.GetAllPlatforms();
+        var platformDtos = _mapper.Map<IEnumerable<PlatformReadDto>>(platforms);
+
+        return Ok(platformDtos);
+    }
+
+    [HttpGet("{id}", Name = "GetPlatformById")]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatformById(int id)
+    {
+        Console.Write("--> Getting Platforms...");
+
+        var platform = _repository.GetPlatformById(id);
+        
+        if(platform != null)
+        {
+            var platformDto = _mapper.Map<PlatformReadDto>(platform);
+
+            return Ok(platformDto);
+        }
+
+        return NotFound();
+    }
+
+    [HttpPost]
+    public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+    {
+        Console.Write("--> Getting Platforms...");
+
+        var platformModel = _mapper.Map<Platform>(platformCreateDto);
+
+        _repository.CreatePlatform(platformModel);
+        _repository.SaveChanges();
+
+        var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+        return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
+    }
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -4,17 +4,15 @@ using PlatformService.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseInMemoryDatabase("InMem");
 });
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
-
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
