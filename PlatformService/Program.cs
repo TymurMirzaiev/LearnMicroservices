887a2702fac72a69efef932da81f63ad3c62a1b5
Tymur_Mirzaiev <tymur.mirzaiev@wolterskluwer.com>
2022-09-18 18:14:48 +0300

    add in memory db, seeding db

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseInMemoryDatabase("InMem");
+});
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
 
 var app = builder.Build();
 
@@ -21,5 +30,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+PrepDb.PrepPopulation(app);
 
 app.Run();
