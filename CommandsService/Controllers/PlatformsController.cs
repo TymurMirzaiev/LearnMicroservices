c82d56482de09a40f25aa4dc36d6023ac67bc3fd
Tymur_Mirzaiev <tymur.mirzaiev@wolterskluwer.com>
2022-10-11 12:17:47 +0300

    save

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers.PlatformsController;
 
@@ -6,9 +9,23 @@ namespace CommandsService.Controllers.PlatformsController;
 [ApiController]
 public class PlatformsController : ControllerBase
 {
-    public PlatformsController()
-    {
+    private readonly ICommandRepo _commandRepo;
+    private readonly IMapper _mapper;
 
+    public PlatformsController(ICommandRepo commandRepo, IMapper mapper)
+    {
+        _commandRepo = commandRepo;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+        Console.WriteLine("--> Getting Platforms from CommandsService");
+
+        var platforms = _commandRepo.GetAllPlatforms();
+
+        return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
     }
 
     [HttpPost]
